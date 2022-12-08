e46b854e68ed99b45332df6d022af588f2f3809d
dangnv1994 <34670554+dangnv1994@users.noreply.github.com>
2022-12-08 22:22:50 +0700

    add code

---
+++ PKBsHue/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PKBsHue.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
